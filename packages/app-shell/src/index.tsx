115aec6a96a052dcdda044dda7dc7c57645e55de
Florian Rappl <florian.rappl@smapiot.com>
2022-09-01 16:26:37 +0200

    Updated content

--- packages/app-shell/src/index.tsx
+++ packages/app-shell/src/index.tsx
@@ -1,16 +1,10 @@
-import 'piral/polyfills';
-import { renderInstance } from 'piral';
-import { layout, errors } from './layout';
-
-// change to your feed URL here (either using feed.piral.cloud or your own service)
-const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
+import { renderInstance } from "piral";
+import { layout, errors } from "./layout";
 
 renderInstance({
   layout,
   errors,
   requestPilets() {
-    return fetch(feedUrl)
-      .then(res => res.json())
-      .then(res => res.items);
+    return fetch("./pilets.json").then((res) => res.json());
   },
 });
